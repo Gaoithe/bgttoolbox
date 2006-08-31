6bcc086a580b4460074377ed4cacc7255e35df90
jamesc <jamesc>
2006-08-31 20:14:45 +0000

    run-library-check-fionnuala.sh and run- script documented in library-check.pl

---
+++ torture/bin/run-library-check-fionnuala.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BORROWER=D2000000163296
+PIN=2323
+MAILTO="jamesc@dspsrv.com,fionnuala@callan.de"
+MAILPROG="mail -r jamesc@dspsrv.com"
+# my qmail is configured not-so-goodly :-7
+
+export PATH=$PATH:/usr/local/bin
+
+#library-check.pl -M -m "$MAILPROG" $BORROWER $PIN $MAILTO
+library-check.pl -m "$MAILPROG" $BORROWER $PIN $MAILTO
