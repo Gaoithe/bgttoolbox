19478e1f163a401a3cd4e54aaaa7da73d6735a1b
gaoithe <jamesc@dspsrv.com>
2014-06-05 15:23:29 +0100

    add some tests, c and java

---
+++ src/c_misc/DirProcess.java
@@ -0,0 +1,130 @@
+
+/***
+https://issues.jenkins-ci.org/browse/JENKINS-23234?focusedCommentId=202596#comment-202596
+Regular cvs behaviour (generally but upon update in particular) is to ignore symlinks.
+See function special_file_mismatch in update.c (and function islink in filesubr.c) in cvs source code.
+http://cvs.savannah.gnu.org/viewvc/cvs/ccvs/src/update.c?revision=HEAD&view=markup
+
+
+https://github.com/jenkinsci/cvs-plugin/blob/master/src/main/java/hudson/scm/AbstractCvs.java
+Nope. Can't do it in CVS Plugin
+CVS Plugin just uses netbeans cvs lib.
+
+https://versioncontrol.netbeans.org/javacvs/library/
+http://hg.netbeans.org/core-main/file/0361f0f86dbb/lib.cvsclient/src/org/netbeans/lib/cvsclient/command/update
+http://hg.netbeans.org/core-main/file/0361f0f86dbb/lib.cvsclient/src/org/netbeans/lib/cvsclient/command/BasicCommand.java
+ setFiles . . no.
+ addRequestsForDirectory(File directory)
+
+In Java there is not a good way of testing for symbolic links :-P
+http://stackoverflow.com/questions/813710/java-1-6-determine-symbolic-links
+
+???
+http://docs.oracle.com/javase/tutorial/essential/io/fileOps.html
+The Files class is "link aware." Every Files method either detects what to do when a symbolic link is encountered, or it provides an option enabling you to configure the behavior when a symbolic link is encountered.
+http://docs.oracle.com/javase/tutorial/essential/io/links.html
+As mentioned previously, the java.nio.file package, and the Path class in particular, is "link aware." 
+Detecting a Symbolic Link
+
+To determine whether a Path instance is a symbolic link, you can use the isSymbolicLink(Path) method. The following code snippet shows how:
+
+Path file = ...;
+boolean isSymbolicLink =
+    Files.isSymbolicLink(file);
+
+
+
+
+test
+[james@nebraska c]$ java DirProcess
+File: ims, is sym link? false
+File: ., is sym link? true
+File: file.txt, is sym link? false
+File: symlink, is sym link? false
+[james@nebraska c]$ touch file.txt
+[james@nebraska c]$ ln -s file.txt symlink
+[james@nebraska c]$ mkdir ims
+[james@nebraska c]$ java DirProcess
+File: ims, is sym link? false
+File: ., is sym link? true
+File: file.txt, is sym link? false
+File: symlink, is sym link? true
+
+
+How do we build libcvsclient ?? Part of CVS plugin that does the cvs update. libcvsclient == part of netbeans.
+Can build from jenkins source?
+Or netbeans source?
+Or mclarke forked netbeans libcvsclient and there is a github repo.
+
+https://github.com/mc1arke/cvsclient/
+https://github.com/mc1arke/cvsclient/tree/master/src/main/java/org/netbeans/lib/cvsclient/command
+
+***/
+
+//package jco.testsymlink;
+
+import java.io.File;
+import java.io.*;
+import java.util.*;
+//import java.nio.file;
+
+// import org.netbeans.lib.cvsclient.*;
+
+public class DirProcess {
+
+    public static boolean isSymlink(File file) throws IOException {
+        if (file == null)
+             throw new NullPointerException("File must not be null");
+        File canon;
+        if (file.getParent() == null) {
+            canon = file;
+        } else {
+            File canonDir = file.getParentFile().getCanonicalFile();
+            canon = new File(canonDir, file.getName());
+        }
+        return !canon.getCanonicalFile().equals(canon.getAbsoluteFile());
+    }
+    
+    public static void main(String [ ] args)
+    {
+	boolean b1;
+	try {
+	    b1 = isSymlink(new File("ims"));
+	    System.err.println("File: ims, is sym link? " + b1);
+	}
+	catch (IOException ex) {
+	    System.err.println("File check error. " + ex);
+	    ex.printStackTrace();
+	}
+
+	try {
+	    b1 = isSymlink(new File("."));
+	    System.err.println("File: ., is sym link? " + b1);
+	}
+	catch (IOException ex) {
+	    System.err.println("File check error. " + ex);
+	    ex.printStackTrace();
+	}
+
+	try {
+	    b1 = isSymlink(new File("file.txt"));
+	    System.err.println("File: file.txt, is sym link? " + b1);
+	}
+	catch (IOException ex) {
+	    System.err.println("File check error. " + ex);
+	    ex.printStackTrace();
+	}
+
+	try {
+	    b1 = isSymlink(new File("symlink"));
+	    System.err.println("File: symlink, is sym link? " + b1);
+	}
+	catch (IOException ex) {
+	    System.err.println("File check error. " + ex);
+	    ex.printStackTrace();
+	}
+
+    }
+
+}
+
---
+++ src/c_misc/TC_mas_contact_gruu.c
@@ -0,0 +1,318 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+//#define SBUG_SOME printf
+//#define SBUG_SOME(format, ...) { printf("%s:%d ",__FILE__,__LINE__); printf(format, ## __VA_ARGS__); printf("\n"); }
+#define SBUG_SOME(format, ...) { printf("%s:%d ",__FILE__ ":" ,__LINE__); printf(format, ## __VA_ARGS__); printf("\n"); }
+
+#include <string.h>
+#define STRDUP strdup
+#define RESTRDUP(a,b) strdup(b)
+
+
+
+void do_contact_test(char *contact_from_invite)
+{
+// void mcs_ac_set_contact_uri_fn(struct mcs_state_machine_instance *mcs_smi)   from mas/ims/chat_session_actions.c
+
+    const char *tmpPtr = NULL;
+    static char *tokBuf = NULL;
+    char *tokStr = NULL;
+    char *contact = NULL;
+
+    tmpPtr = contact_from_invite; //imf_hdr_get(mcs_smi->oa_invite_imf, corrib_sip_ihd_contact_num, 0);
+        if (strlen(tmpPtr) > 0) {
+	    SBUG_SOME("contact from invite [%s]", tmpPtr);
+            tokBuf = RESTRDUP(tokBuf, tmpPtr);
+            if ((tokStr = strstr(tokBuf, "sip:")) != NULL) {
+
+                contact = strtok(tokStr, ":>");
+                SBUG_SOME("1st token [%s]", contact);
+                if ((contact = strtok(NULL, ":>")) != NULL)
+                {
+                    SBUG_SOME("Contact [%s]", contact);
+                    printf("Result:\nContact: <sip:%s>", contact);
+                    //mcs_smi->contact_uri = STRDUP(contact);
+                }
+            }
+            else {
+                // Is this default correct ?
+                //mcs_smi->contact_uri = STRDUP(mcs_smi->app_server->as_uri);
+                SBUG_SOME("No sip: in contact, using as_uri");
+            }
+        }
+}
+
+int main(int argc, char**argv)
+{
+    char *contact_from_invite = "Contact: <sip:+353894017257@omn-ims.test;gr=urn:gsma:imei:35592104-359095-7>;+g.oma.sip-im";
+    do_contact_test(contact_from_invite);
+
+    if (argc>=2) {
+	char *c = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	FILE *f=fopen(argv[1],"r");
+	if (f == NULL)
+	    exit(-3);
+
+	while ((read = getline(&c, &len, f)) != -1) {
+	    printf("contact length %zu :\n", read);
+	    printf("%s\n", c);
+	    do_contact_test(c);
+	}
+	if (c) free(c);   // free here - getline does the initial malloc, realloc if needed during loops
+	
+	fclose(f);
+    }
+    return 0;
+}
+
+
+#if 0
+
+Hi Garv,
+
+Thanks for pointing directly to where change is needed :)
+ from chat_session_actions.c in mcs_ac_set_contact_uri_fn the contact is set . . .
+
+Simple change I think but it probably is in a place which could break things so I've looked at a selection of Contact: samples.
+The main thing is we want to strip out :<port> after the <user>@<server> part in sip and we want to keep all parameters.
+I assume we wish to keep all parameters inside <> and outside.
+Right? :)
+
+The intent(I think) is to just strip off the :<port> in sip contact.
+The strtok looks for ":>" and anything after either : or > is stripped. 
+2 problems:
+1. any param with : is corrupted (the gr=urn param contains : so it is chopped).
+2. If there is a :<port> then all parameters after it (and before >) are stripped too. Probably not intended.
+Later on everything after > is put back
+(from o_invite.c str_create_invite the contact that is set is used and anything after the > is added back)
+
+The way to fix is do first strtok on ";>" instead of ":>" . . .
+Then strip off :<port> from the first part.
+Then add anything that was between ; and > if there was something there.
+
+
+
+
+i.e. with the gruu contact 
+Contact: <sip:+353894017257@omn-ims.test;gr=urn:gsma:imei:35592104-359095-7>;+g.oma.sip-im
+Which do we want?
+Contact: <sip:+353894017257@omn-ims.test>
+Contact: <sip:+353894017257@omn-ims.test;gr=urn:gsma:imei:35592104-359095-7>
+Contact: <sip:+353894017257@omn-ims.test;gr=urn:gsma:imei:35592104-359095-7>;+g.oma.sip-im   <=  I think we want this one.
+What we get:
+Contact: <sip:+353894017257@omn-ims.test;gr=urn>;+g.oma.sip-im
+
+e.g. for other contacts
+Contact: <sip:10.220.105.213:60860>
+Result:
+Contact: <sip:10.220.105.213>
+no problem
+
+Contact: <sip:10.220.105.213:5054>;expires=300
+Result:
+Contact: <sip:10.220.105.213>;expires=300
+no problem
+
+Contact: <sip:+353861953134@192.168.127.239:48865;ob>;q=0.5;+sip.instance="<urn:gsma:imei:35287606-388013-9>";+g.3gpp.cs-voice;+g.3gpp.iari-ref="urn%3Aurn-7%3A3gpp-application.ims.iari.gsma-is";+g.3gpp.icsi-ref="urn%3Aurn-7%3A3gpp-service.ims.icsi.oma.cpm.msg";+g.oma.sip-im
+Result: (;ob unintentionally stripped)
+Contact: <sip:+353861953134@192.168.127.239>;q=0.5;+sip.instance="<urn:gsma:imei:35287606-388013-9>";+g.3gpp.cs-voice;+g.3gpp.iari-ref="urn%3Aurn-7%3A3gpp-application.ims.iari.gsma-is";+g.3gpp.icsi-ref="urn%3Aurn-7%3A3gpp-service.ims.icsi.oma.cpm.msg";+g.oma.sip-im
+
+I can see this heppened on my clearwater setup:
+from clearwater(sprout) to mas:
+Contact: <sip:+353894017258@192.168.127.78:55750;ob>;+g.oma.sip-im;+sip.instance="<urn:gsma:imei:35592104-358960-3>"
+from mas to clearwater(sprout) on other leg:
+Contact: <sip:+353894017258@192.168.127.78>;+g.oma.sip-im;+sip.instance="<urn:gsma:imei:35592104-358960-3>"
+
+
+Interesting side-note I see this in a clearwater register response.
+Handsets handle these responses fine I think.
+1. sip in Contact: without <>s at all at all at all
+2. interesting to see that : is htmlerized (%3A) in some strings 
+REGISTER: 
+Contact: <sip:+353894017258@192.168.127.78:55750;ob>;q=0.5;+sip.instance="<urn:gsma:imei:35592104-358960-3>";+g.3gpp.cs-voice;+g.3gpp.iari-ref="urn%3Aurn-7%3A3gpp-application.ims.iari.gsma-is";+g.3gpp.icsi-ref="urn%3Aurn-7%3A3gpp-service.ims.icsi.oma.cpm.msg";+g.oma.sip-im
+REGISTER 200 ok:  (expires=300 is added and <>s are gone)
+Contact: sip:+353894017258@192.168.127.78:41876;ob;q=0.5;expires=300;+sip.instance="<urn:gsma:imei:35592104-358960-3>";+g.3gpp.cs-voice;+g.3gpp.iari-ref="urn%3Aurn-7%3A3gpp-application.ims.iari.gsma-is";+g.3gpp.icsi-ref="urn%3Aurn-7%3A3gpp-service.ims.icsi.oma.cpm.msg";+g.oma.sip-im
+
+
+code to change:
+
+            if ((tokStr = strstr(tokBuf, "sip:")) != NULL) {
+
+                contact = strtok(tokStr, ":>");
+                SBUG_SOME("1st token [%s]", contact);
+                if ((contact = strtok(NULL, ":>")) != NULL)
+                {
+                    SBUG_SOME("Contact [%s]", contact);
+                    printf("Result:\nContact: <sip:%s>", contact);
+                    //mcs_smi->contact_uri = STRDUP(contact);
+                }
+            }
+
+            if ((tokStr = strstr(tokBuf, "sip:")) != NULL) {
+
+                contact = strtok(tokStr, ";>");
+                SBUG_SOME("1st token [%s]", contact);
+                if ((contact = strtok(NULL, ":>")) != NULL)
+                {
+                    SBUG_SOME("Contact [%s]", contact);
+                    printf("Result:\nContact: <sip:%s>", contact);
+                    //mcs_smi->contact_uri = STRDUP(contact);
+                }
+            }
+
+
+
+
+Contact: <sip:+353894017258@192.168.127.78:55750;ob>;q=0.5;+sip.instance="<urn:gsma:imei:35592104-358960-3>";+g.3gpp.cs-voice;+g.3gpp.iari-ref="urn%3Aurn-7%3A3g
+pp-application.ims.iari.gsma-is";+g.3gpp.icsi-ref="urn%3Aurn-7%3A3gpp-service.ims.icsi.oma.cpm.msg";+g.oma.sip-im
+Expires: 600000
+Allow: PRACK, INFO, INVITE, ACK, BYE, CANCEL, UPDATE, SUBSCRIBE, NOTIFY, REFER, MESSAGE, OPTIONS
+Supported: gruu
+Authorization: Digest username="+353894017258@openims.test", realm="openims.test", nonce="7549197f398abaa6", uri="sip:openims.test", response="7cb3cc52a3f1b3a39
+abd4f5fa16a16c5", algorithm=MD5, opaque="1f1ea06f7e4a0fbd"
+Route: <sip:10.124.51.133:5054;transport=TCP;lr;orig>
+Content-Length:  0
+
+
+--end msg--
+03-03-2014 18:03:36.028 Error httpconnection.cpp:390: http://localhost:7253/timers failed at server 127.0.0.1 : HTTP response code said error (22 405) : fatal
+03-03-2014 18:03:36.029 Error httpconnection.cpp:429: cURL failure with cURL error code 22 (see man 3 libcurl-errors) and HTTP error code 405
+03-03-2014 18:03:36.029 Verbose stack.cpp:237: TX 1009 bytes Response msg 200/REGISTER/cseq=61712 (tdta0x7fcd0c39e6b0) to TCP 10.124.51.133:44716:
+--start msg--
+
+SIP/2.0 200 OK
+Service-Route: <sip:10.124.51.133:5054;transport=TCP;lr;orig>
+Via: SIP/2.0/TCP 10.124.51.133:44716;rport=44716;received=10.124.51.133;branch=z9hG4bKPjX2g5v7r-p45vl7rUMHV6w5Di6pwYt7rV
+Via: SIP/2.0/UDP 192.168.127.78:55750;rport=55750;received=89.101.214.194;branch=z9hG4bKPjYGUiSsSlFIOsVi38eqRkL74iPAH3fqft
+Call-ID: QEDja3BlVsbqlQuha2qzlJrPSRBgyJke
+From: <sip:+353894017258@openims.test>;tag=xQmMC-Bn51FoY6-SXwMg0AF57B4jMB3i
+To: <sip:+353894017258@openims.test>;tag=z9hG4bKPjX2g5v7r-p45vl7rUMHV6w5Di6pwYt7rV
+CSeq: 61712 REGISTER
+Supported: outbound
+Contact: sip:+353894017258@192.168.127.78:55750;ob;q=0.5;expires=300;+sip.instance="<urn:gsma:imei:35592104-358960-3>";+g.3gpp.cs-voice;+g.3gpp.iari-ref="urn%3Aurn-7%3A3gpp-application.ims.iari.gsma-is";+g.3gpp.icsi-ref="urn%3Aurn-7%3A3gpp-service.ims.icsi.oma.cpm.msg";+g.oma.sip-im
+
+
+
+
+
+
+
+
+
+[james@nebraska mas]$ gcc /home/james/c/TC_mas_contact_gruu.c -o /home/james/c/TC_mas_contact_gruu
+[james@nebraska mas]$ find ../.. -name "*.h" -exec grep RESTRDUP {} +^C
+[james@nebraska mas]$ ^C
+[james@nebraska mas]$ /home/james/c/TC_mas_contact_gruu
+/home/james/c/TC_mas_contact_gruu.c::24 contact from invite [Contact: <sip:+353894017257@omn-ims.test;gr=urn:gsma:imei:35592104-359095-7>;+g.oma.sip-im]
+/home/james/c/TC_mas_contact_gruu.c::29 1st token [sip]
+/home/james/c/TC_mas_contact_gruu.c::32 Contact [+353894017257@omn-ims.test;gr=urn]
+
+
+
+
+
+from o_invite.c str_create_invite the contact that is set is used and anything after the > is added back:
+
+    // Contact
+    c1 = imf_hdr_get(oa_imf, corrib_sip_ihd_contact_num, 0);
+    if(mcs_smi->contact_uri){
+        SBUG_SOME("Contact URI already [%s]", mcs_smi->contact_uri);
+
+        str = tbx_strcatf(str, "Contact: <sip:%s>", mcs_smi->contact_uri);
+
+        // copy extra bits from original contact.
+        c1 = imf_hdr_get(oa_imf, corrib_sip_ihd_contact_num, 0);
+        if(((c2 = strchr(c1, '>')) != NULL) ||
+                ((c2 = strchr(c1, ';')) != NULL)){
+            str = tbx_strcat(str, (*c2 == ';') ? c2 : c2+1);
+        }
+        str = tbx_strcat(str, "\r\n"); 
+    }
+    else {
+        str = tbx_strcatf(str, "Contact: %s\r\n", c1);
+        SBUG_SOME("Contact set to contact from oa_imf [%s]", c1);
+    }
+
+
+
+
+
+// ../../libtbx/libtbx_inc/mem.h:#define RESTRDUP(a, b)     tbx_restrdup(a, b, __FILE__ ":" __LINE_STR__, __LINE__)
+//#define STRDUP(a)          tbx_strdup(a, __FILE__ ":" __LINE_STR__, __LINE__)
+//#define TMPSTRDUP(a)       tbx_tmpstrdup(a, __FILE__ ":" __LINE_STR__, __LINE__)
+//#define RESTRDUP(a, b)     tbx_restrdup(a, b, __FILE__ ":" __LINE_STR__, __LINE__)
+
+
+
+char *tbx_strdup(const char *str,
+                 char *file,
+                 int line)
+{
+    char *new_str ;
+    int len;
+
+    tbx_strdup_count++;
+
+    if(str != tbx_empty_string){
+        len = strlen(str) + 1;
+        new_str = tbx_malloc(len, file, line);
+        memcpy(new_str, str, len);
+    }
+    else{
+        new_str = (char *)tbx_empty_string;
+    }
+    
+    return new_str;
+}
+
+#define TBX_TMP_STR_ARR_SIZE 128
+static char *tmp_str_arr[TBX_TMP_STR_ARR_SIZE] = {
+    NULL,
+};
+
+static int tmp_str_arr_idx = 0;
+
+char *tbx_tmpstrdup(const char *str,
+                    char *file,
+                    int line)
+{
+    char *new_str, *old_str;
+
+    new_str = tbx_strdup(str, file, line);
+
+    if((old_str = tmp_str_arr[tmp_str_arr_idx]) != NULL){
+        tbx_free(old_str, file, line);
+    }
+
+    tmp_str_arr[tmp_str_arr_idx++] = new_str;
+
+    if(tmp_str_arr_idx == TBX_TMP_STR_ARR_SIZE){
+        tmp_str_arr_idx = 0;
+    }
+    
+    return new_str;
+}
+
+char *tbx_restrdup(char *old,
+                   const char *str,
+                   char *file,
+                   int line)
+{
+    char *new_str;
+
+    new_str = tbx_strdup(str, file, line);
+
+    if(old != NULL){
+        tbx_free(old, file, line);
+    }
+    
+    return new_str;
+}
+
+
+
+#endif
