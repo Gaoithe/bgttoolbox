eaa5cd0ef925ebd7dbb95ab1f61239b2648f97d9
gaoithe <jamesc@dspsrv.com>
2015-01-29 12:26:18 +0000

    run_start_prov.sh and run_uninstall_install_TC_master*.sh make more generic can pass host name(s) to scripts

--- bin/run_start_prov.sh
+++ bin/run_start_prov.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
+HOST1=$1
+[[ "$HOST1" == "" ]] && HOST1=vb-28
+
+HOST2=$2
+[[ "$HOST2" == "" ]] && HOST2=vb-48
+
 ## SETUP .ssh keys as vroot and omn user on each vbox and place in your authorized keys 
 #[omn@vb-28] ssh-keygen -t rsa
 #[omn@vb-28] cat ~/.ssh/id_rsa.pub     # place this pub key into ~james/.ssh/authorized_keys
 #[omn@vb-28] vi ~/.ssh/authorized_keys #place local pub key(by ssh-keygen) and remote pub key in here 
 
 
-   #scp ~/bin/run_uninstall_TC.sh omn@vb-28:scripts/
-   #scp ~/bin/run_uninstall_TC.sh omn@vb-48:scripts/
-   #scp ~/bin/run_install_TC.sh omn@vb-28:scripts/
-   #scp ~/bin/run_install_TC.sh omn@vb-48:scripts/
-   scp ~/bin/run_solo_start_TC.sh omn@vb-28:scripts/
-   scp ~/bin/run_rejoin_TC.sh omn@vb-48:scripts/
-   scp ~/bin/run_provision_TC.sh omn@vb-28:scripts/
+#scp ~/bin/run_uninstall_TC.sh omn@${HOST1}:scripts/
+#scp ~/bin/run_uninstall_TC.sh omn@${HOST2}:scripts/
+#scp ~/bin/run_install_TC.sh omn@${HOST1}:scripts/
+#scp ~/bin/run_install_TC.sh omn@${HOST2}:scripts/
+scp ~/bin/run_solo_start_TC.sh omn@${HOST1}:scripts/
+scp ~/bin/run_rejoin_TC.sh omn@${HOST2}:scripts/
+scp ~/bin/run_provision_TC.sh omn@${HOST1}:scripts/
 
-   #ssh vroot@vb-28 /apps/omn/scripts/run_uninstall_TC.sh |tee -a TC_uninstall.log; 
-   #ssh vroot@vb-48 /apps/omn/scripts/run_uninstall_TC.sh |tee -a TC_uninstall.log; 
+#ssh vroot@${HOST1} /apps/omn/scripts/run_uninstall_TC.sh |tee -a TC_uninstall.log; 
+#ssh vroot@${HOST2} /apps/omn/scripts/run_uninstall_TC.sh |tee -a TC_uninstall.log; 
 
-   # TODO, how to run these in parallel ?
-   #ssh vroot@vb-28 /apps/omn/scripts/run_install_TC.sh |tee -a TC_install.log; 
-   #ssh vroot@vb-48 /apps/omn/scripts/run_install_TC.sh |tee -a TC_install.log; 
+# TODO, how to run these in parallel ?
+#ssh vroot@${HOST1} /apps/omn/scripts/run_install_TC.sh |tee -a TC_install.log; 
+#ssh vroot@${HOST2} /apps/omn/scripts/run_install_TC.sh |tee -a TC_install.log; 
 
-   ssh omn@vb-28 /apps/omn/scripts/run_solo_start_TC.sh |tee -a TC_solo_start.log; 
-   ssh omn@vb-48 /apps/omn/scripts/run_rejoin_TC.sh |tee -a TC_rejoin.log; 
-   ssh omn@vb-28 /apps/omn/scripts/run_provision_TC.sh |tee -a TC_provision.log; 
+ssh omn@${HOST1} /apps/omn/scripts/run_solo_start_TC.sh |tee -a TC_solo_start.log; 
+ssh omn@${HOST2} /apps/omn/scripts/run_rejoin_TC.sh |tee -a TC_rejoin.log; 
+ssh omn@${HOST1} /apps/omn/scripts/run_provision_TC.sh |tee -a TC_provision.log; 
 
 
--- bin/run_uninstall_install_TC_master.sh
+++ bin/run_uninstall_install_TC_master.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
+HOST=$1
+[[ "$HOST" == "" ]] && HOST=vb-28
+
 ## SETUP .ssh keys as vroot and omn user on each vbox and place in your authorized keys 
 #[omn@vb-28] ssh-keygen -t rsa
 #[omn@vb-28] cat ~/.ssh/id_rsa.pub     # place this pub key into ~james/.ssh/authorized_keys
 #[omn@vb-28] vi ~/.ssh/authorized_keys #place local pub key(by ssh-keygen) and remote pub key in here 
 
 
-   scp ~/bin/run_uninstall_TC.sh omn@vb-28:scripts/
-   scp ~/bin/run_uninstall_TC.sh omn@vb-48:scripts/
-   scp ~/bin/run_install_TC.sh omn@vb-28:scripts/
-   scp ~/bin/run_install_TC.sh omn@vb-48:scripts/
-   scp ~/bin/run_solo_start_TC.sh omn@vb-28:scripts/
-   scp ~/bin/run_rejoin_TC.sh omn@vb-48:scripts/
-   scp ~/bin/run_provision_TC.sh omn@vb-28:scripts/
+scp ~/bin/run_uninstall_TC.sh omn@${HOST}:scripts/
+scp ~/bin/run_install_TC.sh omn@${HOST}:scripts/
+#scp ~/bin/run_rejoin_TC.sh omn@${HOST}:scripts/
+scp ~/bin/run_solo_start_TC.sh omn@${HOST}:scripts/
+scp ~/bin/run_provision_TC.sh omn@${HOST}:scripts/
 
-   ssh vroot@vb-28 /apps/omn/scripts/run_uninstall_TC.sh |tee -a TC_uninstall.log; 
-   #ssh vroot@vb-48 /apps/omn/scripts/run_uninstall_TC.sh |tee -a TC_uninstall.log; 
+ssh vroot@${HOST} /apps/omn/scripts/run_uninstall_TC.sh |tee -a TC_uninstall.log; 
+#ssh vroot@vb-48 /apps/omn/scripts/run_uninstall_TC.sh |tee -a TC_uninstall.log; 
 
-   # TODO, how to run these in parallel ?
-   ssh vroot@vb-28 /apps/omn/scripts/run_install_TC.sh |tee -a TC_install.log; 
-   #ssh vroot@vb-48 /apps/omn/scripts/run_install_TC.sh |tee -a TC_install.log; 
+# TODO, how to run these in parallel ?
+ssh vroot@${HOST} /apps/omn/scripts/run_install_TC.sh |tee -a TC_install.log; 
+#ssh vroot@vb-48 /apps/omn/scripts/run_install_TC.sh |tee -a TC_install.log; 
 
-   #ssh omn@vb-28 /apps/omn/scripts/run_solo_start_TC.sh |tee -a TC_solo_start.log; 
-   #ssh omn@vb-48 /apps/omn/scripts/run_rejoin_TC.sh |tee -a TC_rejoin.log; 
-   #ssh omn@vb-28 /apps/omn/scripts/run_provision_TC.sh |tee -a TC_provision.log; 
+#ssh omn@${HOST} /apps/omn/scripts/run_solo_start_TC.sh |tee -a TC_solo_start.log; 
+#ssh omn@vb-48 /apps/omn/scripts/run_rejoin_TC.sh |tee -a TC_rejoin.log; 
+#ssh omn@${HOST} /apps/omn/scripts/run_provision_TC.sh |tee -a TC_provision.log; 
 
 
