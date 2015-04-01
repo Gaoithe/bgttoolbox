3e5e27bbd70c468e4530b196c6c61db542a0322c
gaoithe <jamesc@dspsrv.com>
2015-04-01 12:54:40 +0100

    build_greperr.sh add java errors

--- bin/build_greperr.sh
+++ bin/build_greperr.sh
@@ -2,7 +2,7 @@ LOGFILE=builderr.log
 echo "\nDATE=$(date)" |tee -a builderr.log
 tail -3 plv-Master.log |tee -a builderr.log
 #grep -C7 -E "Error|failed|oikes" plv*.log |tee -a builderr.log
-grep -B1 -A3 -E "Error | failed|Zoikes|Yikes|No such|workareas" plv*.log |grep -Ev " Delete "|tee -a builderr.log
+grep -B1 -A3 -E "Error | failed|Zoikes|Yikes|No such|workareas|\[javac\]" plv*.log |grep -Ev " Delete "|tee -a builderr.log
 
 # logrotate not working
 #cat > builderr.logrotate.config <<EOF
