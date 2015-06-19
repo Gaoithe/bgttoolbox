9cb022eefd37e7bd634e4b85be8cbe606d774a35
gaoithe <jamesc@dspsrv.com>
2015-06-19 15:18:56 +0100

    build.sh - jdk version settings and modvers

--- bin/build.sh
+++ bin/build.sh
@@ -9,6 +9,11 @@ echo "just for fun" > PURPOSE
 /home/james/bin/buildall.sh co 
 #/homes/brian/scripts/buildall.sh co 
 
+# after fixing up mod.list and mod.graph with new module hammer-x/drill/ipdip/server . . .
+# then run buildall.sh modvers from top [[optionally with -start hammer-x/drill/ipdip/server]]
+/home/james/bin/buildall.sh modvers
+#[james@nebraska TCHOD4]$ buildall.sh -oneshot hammer-x/drill/custard/server modvers
+
 ## apply some changes needed for building
 touch SBUG_SRC_BASE
 touch SCM.BUILD.ALL
@@ -22,5 +27,11 @@ FAKEY_SUPPORTED_LINUX=Y
 FAKEY_PRETEND_VERSION=FC9
 FAKEY_REAL_VERSION=F19
 EOF
+
+export JDK_VERSION=1.7
+export JAVA_SOURCE_VERSION=1.7
+export JAVA_TARGET_VERSION=1.7
+unset JAVA
+unset JAVA_HOME
 
 nice time /homes/brian/scripts/buildmonster.pl 
