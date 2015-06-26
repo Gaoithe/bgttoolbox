ac636eba439db145e515ad4bd4404dd1c68e4ae9
gaoithe <jamesc@dspsrv.com>
2015-06-26 16:47:38 +0100

    build.sh branch checkout/build

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-#echo "Building mod.list"
-#/slingshot/sbe/LATEST/scripts/build_order --cvs-modules --root /slingshot/deployments/OMN-Traffic-Control >mod.list
-#echo "Building mod.graph"
-#/slingshot/sbe/LATEST/scripts/build_order --digraph --include-non-metas --root /slingshot/deployments/OMN-Traffic-Control >mod.graph
+
+
+BRANCH=OMN-Traffic-Control-14-Q3
+
+echo "Building mod.list"
+if [[ -z $BRANCH ]] ; then
+   /slingshot/sbe/LATEST/scripts/build_order --cvs-modules --root /slingshot/deployments/OMN-Traffic-Control >mod.list
+   echo "Building mod.graph"
+   /slingshot/sbe/LATEST/scripts/build_order --digraph --include-non-metas --root /slingshot/deployments/OMN-Traffic-Control >mod.graph
+else 
+   cp /slingshot/BRANCHES-PLANS/${BRANCH}.plan branch.plan
+   echo "# BRANCH-NAME ${BRANCH}" >mod.list
+   grep -v ^# branch.plan |awk '{print $2}' >>  mod.list
+   echo "Building mod.graph"
+   DTCVER=$(grep deployments/OMN-Traffic-Control mod.list |cut -d" " -f 3 |sed "s/-/\//g")
+   /slingshot/sbe/LATEST/scripts/build_order --digraph --include-non-metas --root /slingshot/deployments/OMN-Traffic-Control/$DTCVER/SRC >mod.graph
+fi
+
+
 echo "just for fun" > PURPOSE
 
 # james machine nebraska doesn't like cvs -Q -z9 co -R whatever # presumably -z9 is the problem. it repeatedly gets stuck :-(
+# buildall.sh gets branch from BRANCH-NAME in mod.list
 /home/james/bin/buildall.sh co 
 #/homes/brian/scripts/buildall.sh co 
 
@@ -13,6 +29,8 @@ echo "just for fun" > PURPOSE
 # then run buildall.sh modvers from top [[optionally with -start hammer-x/drill/ipdip/server]]
 /home/james/bin/buildall.sh modvers
 #[james@nebraska TCHOD4]$ buildall.sh -oneshot hammer-x/drill/custard/server modvers
+
+/home/james/bin/buildall.sh spotless
 
 ## apply some changes needed for building
 touch SBUG_SRC_BASE
