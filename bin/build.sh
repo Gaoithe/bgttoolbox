69a6393293cfff45ee9eba9f172336634af447d5
gaoithe <jamesc@dspsrv.com>
2015-09-16 12:38:47 +0100

    build scripts update

--- bin/build.sh
+++ bin/build.sh
@@ -18,34 +18,37 @@ else
    /slingshot/sbe/LATEST/scripts/build_order --digraph --include-non-metas --root /slingshot/deployments/OMN-Traffic-Control/$DTCVER/SRC >mod.graph
 fi
 
+if [[ $MONSTERFLAGS != "-restart" ]] ; then
 
 echo "just for fun" > PURPOSE
 
-# james machine nebraska doesn't like cvs -Q -z9 co -R whatever # presumably -z9 is the problem. it repeatedly gets stuck :-(
-# buildall.sh gets branch from BRANCH-NAME in mod.list
-/home/james/bin/buildall.sh co 
-#/homes/brian/scripts/buildall.sh co 
+   # james machine nebraska doesn't like cvs -Q -z9 co -R whatever # presumably -z9 is the problem. it repeatedly gets stuck :-(
+   # buildall.sh gets branch from BRANCH-NAME in mod.list
+   /home/james/bin/buildall.sh co 
+   #/homes/brian/scripts/buildall.sh co 
 
-# after fixing up mod.list and mod.graph with new module hammer-x/drill/ipdip/server . . .
-# then run buildall.sh modvers from top [[optionally with -start hammer-x/drill/ipdip/server]]
-/home/james/bin/buildall.sh modvers
-#[james@nebraska TCHOD4]$ buildall.sh -oneshot hammer-x/drill/custard/server modvers
+   # after fixing up mod.list and mod.graph with new module hammer-x/drill/ipdip/server . . .
+   # then run buildall.sh modvers from top [[optionally with -start hammer-x/drill/ipdip/server]]
+   /home/james/bin/buildall.sh modvers
+   #[james@nebraska TCHOD4]$ buildall.sh -oneshot hammer-x/drill/custard/server modvers
 
-[[ $MONSTERFLAGS != "-restart" ]] && /home/james/bin/buildall.sh spotless
+   /home/james/bin/buildall.sh spotless
 
-## apply some changes needed for building
-touch SBUG_SRC_BASE
-touch SCM.BUILD.ALL
-#cp -p {/home/james/work/TC/,}MOS-base/mkrpms.sh 
-#cp -p {/home/james/work/TC/,}libtbx/hostid.c
-#cp -p {/home/james/work/TC/,}libtbx/Makefile
+   ## apply some changes needed for building
+   touch SBUG_SRC_BASE
+   touch SCM.BUILD.ALL
+   #cp -p {/home/james/work/TC/,}MOS-base/mkrpms.sh 
+   #cp -p {/home/james/work/TC/,}libtbx/hostid.c
+   #cp -p {/home/james/work/TC/,}libtbx/Makefile
 
-# see sbe/CHANGES make vars LINUX_VER and LINUX_REAL_VER populated
-cat >Makefile.slingshot.fakeyfakey <<EOF
+   # see sbe/CHANGES make vars LINUX_VER and LINUX_REAL_VER populated
+   cat >Makefile.slingshot.fakeyfakey <<EOF
 FAKEY_SUPPORTED_LINUX=Y
 FAKEY_PRETEND_VERSION=FC9
 FAKEY_REAL_VERSION=F19
 EOF
+
+fi
 
 export JDK_VERSION=1.7
 export JAVA_SOURCE_VERSION=1.7
