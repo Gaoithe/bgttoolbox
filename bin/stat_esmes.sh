f40f682ca8c7eadf7e1cfd4943fb5075302ec5f6
gaoithe <jamesc@dspsrv.com>
2015-11-03 11:31:42 +0000

    per-esme stats from ops CDRs - hourly

---
+++ bin/stat_esmes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+OCDRSDIR=/data/operations_cdrs
+STATDIR=/logs/stats/
+
+ODTS=$(date +%d%m%y%H)
+echo ODTS=$ODTS
+# per-ESME per ERR/state/USSD_text counts:
+grep END_POINT: ${OCDRSDIR}/OPS_CDR_${ODTS}* |sed -r 's/.*DEST_IDNT://;s/\s*PPS_ID:.*PPS_ERR:/ PS_ERR:/;s/\s*SILO:.*END_POINT:/ /;s/\s*USSD_Req_text.*USSD_text:/ USSD_text:/;s/USSD_Req_text.*//' |grep -v ^[0-9] |sort |uniq -c
+
+ODTS=$(date +%d%m%y%H -d "$i hour ago")
+
+for i in $(seq 1 $((24*5))); do 
+   ODTS=$(date +%d%m%y%H -d "$i hour ago"); 
+   LOG=$STATDIR/${ODTS}_esmes
+   if [[ ! -e $LOG ]] ; then
+       echo ODTS=$ODTS |tee $LOG; 
+       grep END_POINT: ${OCDRSDIR}/OPS_CDR_${ODTS}* |sed -r 's/.*DEST_IDNT://;s/\s*PPS_ID:.*PPS_ERR:/ PS_ERR:/;s/\s*SILO:.*END_POINT:/ /;s/\s*USSD_Req_text.*USSD_text:/ USSD_text:/;s/USSD_Req_text.*//' |grep -v ^[0-9] |sort |uniq -c |tee -a $LOG
+   fi 
+done
+
