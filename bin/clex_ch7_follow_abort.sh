d5ae803f315400cea0d3867513733cc1b2a2c555
gaoithe <jamesc@dspsrv.com>
2016-04-05 12:51:52 +0100

    clex_ch7_follow_abort.sh consistent fixe extn naming, gen log files if not exist

--- bin/clex_ch7_follow_abort.sh
+++ bin/clex_ch7_follow_abort.sh
@@ -10,9 +10,28 @@ ENDTIME=
 [[ ! -z $1 ]] && STARTTIME=$1 && shift
 [[ ! -z $1 ]] && ENDTIME="-e $1" && shift
 
-[[ ! -e /tmp/${CH7FILE} ]] && clex -ch 7 -s ${STARTTIME} -e ${ENDTIME} >/tmp/${CH7FILE}
+BASEFILE=${CH7FILE//.ch7/}
+if [[ ! -s /tmp/${CH7FILE} && ! -z /tmp/${CH7FILE} ]] ; then
+    clex -ch 7 -s ${STARTTIME} -e ${ENDTIME} >/tmp/${CH7FILE}
+    clex -ch 0 -s ${STARTTIME} -e ${ENDTIME} >/tmp/${BASEFILE}.ch0
+    clex -ch 2 -s ${STARTTIME} -e ${ENDTIME} >/tmp/${BASEFILE}.ch2
+    clex -ch 3 -s ${STARTTIME} -e ${ENDTIME} >/tmp/${BASEFILE}.ch3
+    rm /tmp/${CH7FILE}.txt
+else 
+    echo CH7 file $CH7FILE already exists.
+    ls -alstr /tmp/${BASEFILE}*
+fi
 [[ ! -e /tmp/${CH7FILE}.txt ]] && cat /tmp/${CH7FILE}  |sdi -tcap -ts >/tmp/${CH7FILE}.txt
+
+BEGINCOUNT=$(grep "begin =" /tmp/${CH7FILE}.txt |wc -l)
+ABORTCOUNT=$(grep "abort =" /tmp/${CH7FILE}.txt |wc -l)
+ENDCOUNT=$(grep "end =" /tmp/${CH7FILE}.txt |wc -l)
+echo "FILE:/tmp/${CH7FILE}.txt Begin:$BEGINCOUNT End:$ENDCOUNT Abort:$ABORTCOUNT .... there will be somewhat less than begin/end count full message flows."
+
 TIDS=$(cat /tmp/${CH7FILE}.txt |grep -A1  -P "abort =|tid =" |grep -A2 abort |grep -vP "^--$|tid|abort" |sed -r 's/.*[TR]x:\s*//;s/\s/\\s/g')
+##[[ ! -e /tmp/${CH7FILE} ]] && clex -ch 7 -s ${STARTTIME} -e ${ENDTIME} >/tmp/${CH7FILE}
+##[[ ! -e /tmp/${CH7FILE}.txt ]] && cat /tmp/${CH7FILE}  |sdi -tcap -ts >/tmp/${CH7FILE}.txt
+##TIDS=$(cat /tmp/${CH7FILE}.txt |grep -A1  -P "abort =|tid =" |grep -A2 abort |grep -vP "^--$|tid|abort" |sed -r 's/.*[TR]x:\s*//;s/\s/\\s/g')
 
 #for t in $TIDS; do echo tid=$t; cat /tmp/${CH7FILE}.txt |grep -P $t ; done
 #[omn@tcussd01 ~]$ for t in $TIDS; do echo tid=$t; IDS=$(cat /tmp/${CH7FILE}.txt |grep -P $t |sed 's/^\[//;s/\]//' |awk '{print $1}' |sort |uniq); echo $IDS; for i in $IDS; do echo i=$i; grep -P "^\[\s*$i\]" /tmp/${CH7FILE}.txt ; done;  done |less
@@ -26,3 +45,4 @@ for t in $TIDS; do echo tid=$t; IDS=$(cat /tmp/${CH7FILE}.txt |grep -B4 -A3 -P "
 #[omn@tcussd01 ~]$ for t in $TIDS; do echo tid=$t; IDS=$(cat /tmp/${CH7FILE}.txt |grep -B4 -A3 -P "$t" |grep -A1 'tid\s=' --no-group-separator |sed 's/^\[//;s/\]//' |awk '{print $1}' |uniq); echo IDS=$IDS; for i in $IDS; do echo i=$i; grep -P "^\[\s*$i\]" /tmp/${CH7FILE}.txt |grep -A1 --no-group-separator -P "(abort|begin|end|kontinue) =|msisdn|ussd_text" ; done;  done |less
 for t in $TIDS; do echo tid=$t; IDS=$(cat /tmp/${CH7FILE}.txt |grep -B4 -A3 -P "$t" |grep -A1 'tid\s=' --no-group-separator |sed 's/^\[//;s/\]//' |awk '{print $1}' |uniq); echo IDS=$IDS; for i in $IDS; do echo i=$i; grep -P "^\[\s*$i\]" /tmp/${CH7FILE}.txt |grep -A1 --no-group-separator -P "(abort|begin|end|kontinue) =|msisdn|ussd_text" ; done;  done |tee /tmp/${CH7FILE}.followabort.summary
 
+#grep -A2 -P "begin =|msisdn =|ussd_text =" /tmp/${CH7FILE}.followabort  |grep -A11 "begin =" |grep -v ^--$ 
