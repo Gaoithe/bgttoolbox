955f7a059d7b20cd73c24a4c096381ba40151aea
gaoithe <jamesc@dspsrv.com>
2017-05-11 18:34:35 +0100

    scout_bearing_to_compasspoint.py

--- bin/scout_bearing_to_compasspoint.py
+++ bin/scout_bearing_to_compasspoint.py
@@ -261,7 +261,7 @@ def print_instructions(quote):
                     easting = pos % 5
                     northing = 5 - ((pos-1)/5)
                 print "#    Record letter @ Easting %d, Northing %d  _______" % (easting,northing)
-                print "#    DEBUG post:{} {} c:{}".format(b,xy[n],c)
+                print "#    DEBUG post:{} ({:1.2f},{:1.2f}) c:{}".format(b,xy[n][0],xy[n][1],c)
                 # next
                 r = n
                 i+=1
@@ -372,3 +372,40 @@ plt.show()
 #      Northing
 #
 ########################################
+
+
+
+
+import curses
+stdscr = curses.initscr()
+curses.start_color()
+#curses.noecho()
+#curses.cbreak()
+#stdscr.keypad(1)
+
+yxmax = stdscr.getmaxyx()
+
+pad = curses.newpad(yxmax)
+#pad = curses.newpad(100, 100)
+#  These loops fill the pad with letters; this is
+# explained in the next section
+for y in range(0, yxmax[0]-10):
+    for x in range(0, yxmax[1]-10):
+        try:
+            pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
+        except curses.error:
+            pass
+
+#  Displays a section of the pad in the middle of the screen
+pad.refresh(0,0, 5,5, yxmax[0]-5,yxmax[1]-5) ## pminrow, pmincol, sminrow, smincol, smaxrow, smaxcol
+
+curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+pad.addstr(0,0, "red/white", curses.color_pair(1))
+pad.addstr(1,10, "red/black", curses.color_pair(2))
+pad.addstr(2,20, "red/blue", curses.color_pair(3))
+pad.refresh(0,0, 5,5, yxmax[0]-5,yxmax[1]-5) ## pminrow, pmincol, sminrow, smincol, smaxrow, smaxcol
+
+#curses.nocbreak(); stdscr.keypad(0); curses.echo()
+curses.endwin()
